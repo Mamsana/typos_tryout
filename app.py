a9fe13a0126cbc56e245ed90b32d5b14e4c4f885
Mamsana <alicemamsana@gmail.com>
2023-02-02 04:54:25 -0800

    trying out

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import os
+import openai
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form["text"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Correct typos in the following text\n\n{text}",
+            temperature=0,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0.5,
+            presence_penalty=0
+        )
+        corrected_text = response["choices"][0]["text"]
+        return render_template("index.html", corrected_text=corrected_text)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
