b22beb6217b74ecad12bf61ee4ef68988945f652
Mamsana <alicemamsana@gmail.com>
2023-02-02 05:36:06 -0800

    key update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import openai
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
