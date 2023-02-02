2abec67d56f63a21f94b19902c93826a5bc205a9
Mamsana <alicemamsana@gmail.com>
2023-02-02 10:19:02 -0800

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, request, render_template
+import requests
 import os
 import openai
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        text = request.form["text"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Correct typos in the following text\n\n{text}",
-            temperature=0,
-            max_tokens=60,
-            top_p=1,
-            frequency_penalty=0.5,
-            presence_penalty=0
-        )
-        corrected_text = response["choices"][0]["text"]
-        return render_template("index.html", corrected_text=corrected_text)
+@app.route('/correct-typo', methods=['GET', 'POST'])
+def correct_typo():
+    if request.method == 'POST':
+        text = request.form['text']
+
+        endpoint = "https://api.openai.com/v1/engines/davinci/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer <API_KEY>"
+        }
+
+        data = {
+            "prompt": text,
+            "max_tokens": 100,
+            "temperature": 0.5
+        }
+    
+        response = requests.post(endpoint, headers=headers, json=data)
+
+        corrected_text = response.json()['choices'][0]['text']
+
+        return render_template('index.html', corrected_text=corrected_text)
+
     return render_template("index.html")
 
 if __name__ == "__main__":
